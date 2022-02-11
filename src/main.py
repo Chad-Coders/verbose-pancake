f422159759d879081ecbef6e6cd212e7d30098a5
AxelBroberg <axellakris2@gmail.com>
2022-02-11 21:24:51 +0100

    Now pushes the entertainment properly

--- src/main.py
+++ src/main.py
@@ -3,17 +3,17 @@ import json
 
 import requests
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_restful import Api, Resource, reqparse
 from justwatch import JustWatch
 
 #print(results['items'])
 #for x in results['items']:
 #    print(x['title'])
-#with open('json_data.json', 'w') as outfile:
-#    json.dump(results['items'], outfile)
+
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def index():
     return "Chad Coders backend API"
@@ -23,7 +23,11 @@ def get():
     my_dict = {"0": []}
     just_watch = JustWatch(country='SE')
     results = just_watch.search_for_item(query='avengers')
-    return jsonify({'data':results['items']})
+    items = results['items']
+    with open('json_data.json', 'w') as outfile:
+        json.dump(items, outfile)
+
+    return jsonify(items)
 
 if __name__ == "__main__":
     app.run(debug=True)
