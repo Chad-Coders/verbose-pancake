8d0743abe548aaa23131d7d48c051c6f849b243e
AxelBroberg <axellakris2@gmail.com>
2022-02-26 15:46:38 +0100

    Stuck on get

--- src/main.py
+++ src/main.py
@@ -13,12 +13,20 @@ CORS(app)
 def index():
     return "Chad Coders backend API"
 
-@app.route('/entertainment', methods=['GET'])
+@app.route('/getdata', methods=['GET'])
 def get():
     just_watch = JustWatch(country='US')
     results = just_watch.search_for_item(query=request.args.get('search'))
     items = results['items']
+    with open('json_data.json', 'w') as outfile:
+        json.dump(items, outfile)
     return jsonify(items)
+
+@app.route('/entertainment', methods=['GET'])
+def getById():
+    just_watch = JustWatch(country='US')
+    results = just_watch.get_title(title_id=request.args.get('id'), content_type='show')
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
