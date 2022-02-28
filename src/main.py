334f7be3fa4ba4c24d01b9ffcc2048befd5afa44
AxelBroberg <axellakris2@gmail.com>
2022-02-28 21:50:31 +0100

    Get works for both show and movie

--- src/main.py
+++ src/main.py
@@ -15,17 +15,27 @@ def index():
 
 @app.route('/getdata', methods=['GET'])
 def get():
-    just_watch = JustWatch(country='US')
+    just_watch = JustWatch(country='SE')
     results = just_watch.search_for_item(query=request.args.get('search'))
     items = results['items']
     with open('json_data.json', 'w') as outfile:
         json.dump(items, outfile)
     return jsonify(items)
 
-@app.route('/entertainment', methods=['GET'])
-def getById():
-    just_watch = JustWatch(country='US')
+@app.route('/show', methods=['GET'])
+def getShow():
+    just_watch = JustWatch(country='SE')
     results = just_watch.get_title(title_id=request.args.get('id'), content_type='show')
+    with open('json_data.json', 'w') as outfile:
+        json.dump(results, outfile)
+    return jsonify(results)
+
+@app.route('/movie', methods=['GET'])
+def getMovie():
+    just_watch = JustWatch(country='SE')
+    results = just_watch.get_title(title_id=request.args.get('id'), content_type='movie')
+    with open('json_data.json', 'w') as outfile:
+        json.dump(results, outfile)
     return jsonify(results)
 
 if __name__ == "__main__":
